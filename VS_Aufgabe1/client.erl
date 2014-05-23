4ba0c551900e8aad01af1f49e2abc5770f59f171
Flo <florian@kletz.de>
2014-05-24 00:32:41 +0200

    Logging and Timer for Client completed (Server Logging , Client Kick and Own Message detection still missing)

--- VS_Aufgabe1/client.erl
+++ VS_Aufgabe1/client.erl
@@ -9,82 +9,147 @@
 -module(client).
 -author("Florian").
 
+-record(status, {
+  ids =werkzeug:emptySL(),
+  config
+}).
 
+
+-record(config, {
+  clients,
+  lifetime,
+  servername,
+  sendeintervall
+}).
 
 %% API
--export([con/1,redakteur/0,leser/0,getNum/0]).
+-export([start/1]).
 
-con(Servername)->
-  net_adm:ping(Servername).
+load_config() ->
+  {ok, ConfigFile} = file:consult('client.cfg'),
+  #config{
+    clients = proplists:get_value(clients, ConfigFile),
+    lifetime = proplists:get_value(lifetime, ConfigFile),
+    servername = proplists:get_value(servername, ConfigFile),
+    sendeintervall = proplists:get_value(sendeintervall, ConfigFile)
+  }.
+
+start(ServerNode) ->
+  net_adm:ping(ServerNode),
+  Config = load_config(),
+  Servername = Config#config.servername,
+  ServerPID = {Servername,ServerNode},
+  log("Client","Servername ~s has Pid: ~p", [Servername, ServerPID]),
+  Lifetime = Config#config.lifetime,
+  %io:format("Lifetime: ~p~n",[Lifetime]),
+  NumClients = Config#config.clients,
+  %io:format("NumClients: ~p~n",[NumClients]),
+  Clients = lists:map(fun(_ClientID) ->
+							ClientPID = spawn(fun() -> redakteur(ServerPID, Config,1) end),
+							log("Client","Client Startzeit: ~p mit PID ~p",[werkzeug:timeMilliSecond(), ClientPID]),
+							timer:exit_after(timer:seconds(Lifetime), ClientPID, "Ende Gelaende")
+						end, lists:seq(1, NumClients)), %TODO:Testing
+  Clients.
 
 %%REDAKTEUR!######################################################
-redakteur()->
-  Number = getNum(),%Nummer organisieren
-  %TODO: TIMER required
-  timer:sleep(1000),
-  send(Number). %Nahricht mit der Nummer schicken
+redakteur(ServerPid,Config,MaxNumbers)->
+  io:format("~n~n"),
+  Number = getNum(ServerPid),%Nummer organisieren
+  io:format("Warte fuer ~p sekunden...~n",[Config#config.sendeintervall]),
+  timer:sleep(timer:seconds(Config#config.sendeintervall)),
+  NewConfig = send(ServerPid,Number,MaxNumbers,Config),%Nahricht mit der Nummer schicken
+  redakteur(ServerPid,NewConfig,MaxNumbers+1). %Endlosschleife starten
 
 
 
 
-getNum() ->
-  Server = global:whereis_name(theserver),
-  io:format("Server PID ~p~n", [Server]),
+
+getNum(Server) ->
+  io:format("Freie Nummer abfragen~n"),
   Server ! { query_msgid, self()},
   receive
     { msgid, Number} ->
-      io:format("Got Number ~w from Server~n", [Number]),
+      log("Client","Nummer ~w zugeteilt durch Server", [Number]),
       Number %Returnvalue
   end.
 
-send(Number) ->
+send(ServerPid,Number,MaxNumbers,Config) ->
   %Drop one out of six
-  case Number rem 5 of
+  case MaxNumbers rem 5 of
     0 ->
       %Vergessen
-      io:format("NO MESSAGE SEND DUE TO RESTRICTION BY MISSION!~n");
+      log("Client","NO MESSAGE SEND DUE TO RESTRICTION BY MISSION!",[]),
+      %Lese Client Starten
+      leser(ServerPid),
+      %Wartezeit neu Setzten
+      setNewWaitingTime(Config);
+
     _ ->
       %Senden
-      Server = global:whereis_name(theserver),
       EigenenNamen="bla",
       RechnerName=net_adm:localhost(),
-      ProzessNummer=io_lib:format("~p", [Server]),
+      ProzessNummer=io_lib:format("~p", [ServerPid]),
       PraktikumsGruppe="02",
       TeamNummer="03",
       AktuelleSystemzeit=werkzeug:timeMilliSecond(),
 
 
       Nachricht=string:join([EigenenNamen,"@",RechnerName,ProzessNummer,PraktikumsGruppe,TeamNummer,AktuelleSystemzeit],"-"),
-      io:format("SENDING: ~p ~s~n", [Number, Nachricht]),
+      log("Client","SENDE: ~p ~s", [Number, Nachricht]),
 
 
-      Server ! {new_message, {Nachricht, Number}},
-      io:format(ok)
+      ServerPid ! {new_message, {Nachricht, Number}},
+      io:format(ok),
+      Config %Unveränderte Config
+
 end.
 
+%% Helper
+setNewWaitingTime(Config)->
+  Intervall = Config#config.sendeintervall,
+  Vorzeichen = case random:uniform() > 0.5 of
+             true -> 1;
+             false -> -1
+           end,
+  NewInterval = round(Intervall + (Intervall/2 * Vorzeichen)),
 
-%%LESER!##################################################
-leser() ->
-  getNext().
+  NewConfig = Config#config{sendeintervall = NewInterval},
 
-
-getNext() ->
-  Server = global:whereis_name(theserver),
-  io:format("Server PID ~p~n", [Server]),
-
-  Server ! { query_messages, self()},
-  receive
-
-    { message, Number,Nachricht,true} -> %%Keine weiteren Nachrichten auf Server
-      io:format(": ~w : ~s : ~w~n", [Number,Nachricht,true]);
-
-    { message, Number,Nachricht,false} -> %% NOCH weitere NAchrichten auf Server
-      io:format(": ~w : ~s : ~w~n", [Number,Nachricht,false]),
-      getNext()
+  case NewConfig#config.sendeintervall < 2 of
+    true -> setNewWaitingTime(NewConfig);
+    false ->
+	log("Client","Neue Wartezeit gesetzt auf: ~p Sekunden~n",[NewInterval]),
+	NewConfig
   end.
 
 
 
+%%LESER!################################################################################################################
+leser(ServerPid) ->
+  log("Client","LESER GESTARTET ~p~n", [ServerPid]),
 
+  getNext(ServerPid).
+
+
+getNext(ServerPid) ->
+  io:format("Server PID ~p | My PID ~p ~n", [ServerPid, self()]),
+
+  ServerPid ! { query_messages, self()},
+  receive
+
+    { message, Number,Nachricht,true} -> %%Keine weiteren Nachrichten auf Server
+      log("Client"," ~w : ~s : ~w", [Number,Nachricht,true]);
+
+    { message, Number,Nachricht,false} -> %% NOCH weitere NAchrichten auf Server
+      log("Client"," ~w : ~s : ~w", [Number,Nachricht,false]),
+      getNext(ServerPid)
+  end.
+
+log(File, Message, Data) ->
+  werkzeug:logging(fileDesc(File), io_lib:format("[~s] (~p) "++Message++"~n",[werkzeug:timeMilliSecond(),self()]++Data)).
+
+fileDesc(File) ->
+  ID = File++string:sub_string(werkzeug:to_String(self()),2,string:len(werkzeug:to_String(self()))-1),
+  ID++".log".
 
 
--- VS_Aufgabe1/myserver.erl
+++ VS_Aufgabe1/myserver.erl
@@ -15,24 +15,37 @@
 -record(status, {
   current_msg_id  = 0,
   hbq =werkzeug:emptySL(),
-  dq = werkzeug:emptySL(),
-  clients=werkzeug:emptySL()
+  dlq = werkzeug:emptySL(),
+  clients=werkzeug:emptySL(),
+  config
 }).
+
+-record(config,{
+  latency,
+  clientlifetime,
+  servername,
+  dlqlimit
+}).
+
+load_config() ->
+  {ok, ConfigFile} = file:consult('server.cfg'),
+  #config{
+    latency = proplists:get_value(latency, ConfigFile),
+    clientlifetime = proplists:get_value(clientlifetime, ConfigFile),
+    servername = proplists:get_value(servername, ConfigFile),
+    dlqlimit = proplists:get_value(dlqlimit, ConfigFile)
+  }.
 
 
 % Server starten und unter Namen "theserver" global registrieren
 % Alternative: Servername aus config Datei lesen
 start() ->
-% {ok, ConfigListe} = file:consult("server.cfg"),
-% {ok, Lifetime} = werkzeug:get_config_value(lifetime, ConfigListe),
-% {ok, HostName} = inet:gethostname(),
-% Datei = lists:concat(["NServer@",HostName,".log"]),
-
-
-  State = #status{current_msg_id = 0},
-
-  Pid = spawn(myserver,server,[State]),
-  global:register_name(theserver,Pid).
+  Config = load_config(),
+  State = #status{ config = Config },
+  ServerPID = spawn(fun() -> server(State) end),
+  io:format("ServerPID=~p~n",[ServerPID]),
+  register(Config#config.servername, ServerPID),
+  ServerPID.
 
 
 
@@ -58,16 +71,42 @@ server(Status) ->
       Return = query_msgid(Status),
       Number = Return#status.current_msg_id,
       From ! {msgid,Number}, %SIGNATUR:  {keyword msgid, Number}
-      server(Return)
+      server(Return);
+
+    Else ->
+      io:format("Cannot handle: ~p", [Else])
 
   end.
 
+%% #####################################################################################################################
+
+%% Gibt die naechste freie ID an einen beliebigen Client.
+query_msgid(State) ->
+  io:format("[query_msgid METHODE]~n"),
+  NewState = State#status{current_msg_id = State#status.current_msg_id + 1},
+  NewState.
+
+%% #####################################################################################################################
+
+%% Empfaengt eine neue Nachricht und Speichert sie in der Holdbackqueue.
+new_message(State,Number,Nachricht) ->
+  io:format("[new_message METHODE]~n"),
+  io:format("Recived: (~w) ~s ~n",[Number,Nachricht]),
+  NachrichtAndTimeStamp = string:join([Nachricht, "|To HBQ:", werkzeug:timeMilliSecond()], " "),
+
+  NewState = State#status{hbq = werkzeug:pushSL(State#status.hbq,{Number,NachrichtAndTimeStamp})},
+  %NewState = State#status{hbq = lists:append(State#status.hbq, {Nachricht})},
+  io:format("[NEW LIST]~p~n",[NewState#status.hbq]),
+  NewState.
+
+%% #####################################################################################################################
+
+%% Liefert Nachrichten an einen anfragenden Client
 query_messages(State,ClientPID) ->
   io:format("~n~n[query_messages METHODE]~n~n"),
-
-  NewState = moveMessagesFromHbqToDq(State),
+  NewState = moveMessagesFromHbqToDlq(State),
   io:format("~n~n[back in query_messages METHODE]~n~n"),
-  io:format("[DQ] ~p~n",[werkzeug:lengthSL(NewState#status.dq)]),
+  io:format("[DLQ] ~p~n",[werkzeug:lengthSL(NewState#status.dlq)]),
   io:format("[HBQ] ~p~n",[werkzeug:lengthSL(NewState#status.hbq)]),
 
   io:format("[werkzeug:findSL(NewState#status.clients,ClientPID)] ~p~n",[werkzeug:findSL(NewState#status.clients,ClientPID)]),
@@ -77,124 +116,95 @@ query_messages(State,ClientPID) ->
 
     {-1,nok} -> %Nicht vorhanden? Initial anlegen
       io:format("[CLIENT NOCH NCIHT VORHANDEN (wird angelegt)] ~n"),
-      %io:format("[dq] ~p~n",[NewState#status.dq]),
+      %io:format("[dlq] ~p~n",[NewState#status.dlq]),
       NewClientTupel = {ClientPID,1}, %Initialisiert mit 1
       TempStatus= NewState#status{clients = werkzeug:pushSL(NewState#status.clients,NewClientTupel)},
-      io:format("[83ClientList]: ~p~n",[TempStatus#status.clients]),
+      io:format("[ClientList]: ~p~n",[TempStatus#status.clients]),
       query_messages(TempStatus,ClientPID);
 
     {_,RequestedIDFromClient} -> %Client in Liste vorhanden
-      io:format("[CLIENT VORHANDEN (getting next id)] ~n"),
+      io:format("[CLIENT VORHANDEN] ~n"),
       io:format("[Client will ID] ~p ~n",[RequestedIDFromClient]),
-      io:format("[DQ] ~p ~n",[NewState#status.dq]),
+      io:format("[DLQ] ~p ~n",[NewState#status.dlq]),
 
 
+      {TempStatus,RealIDToTransfer,RealMessageToTransfer} = case werkzeug:findneSL(NewState#status.dlq,RequestedIDFromClient) of
+                                                             {-1,nok} -> % ID nicht vorhanden also dummy sendne
+                                                                 T2 = "DUMMY MESSAGE (keine neuen Nachrichten vorhanden)",
+                                                                 T1 = NewState,
+                                                                 {T1,RequestedIDFromClient,T2}; %Return
 
-      {TempStatus,RealIDToTransfer,RealMessageToTransfer} = case werkzeug:findneSL(NewState#status.dq,RequestedIDFromClient) of
-                                         {-1,nok} -> % ID nicht vorhanden also dummy sendne
-                                           T2 = "DUMMY MESSAGE (keine neuen Nachrichten vorhanden)",
-                                           T1 = NewState,
-                                           {T1,RequestedIDFromClient,T2}; %Return
+                                                             {IDToTransfer, MessageToTransfer} -> %ID vorhanden IDToTransfer ggf. <= RequestedIDFromClient
+                                                                 {T1,T2,T3} = case MessageToTransfer of
+                                                                            {Message,_,_} -> %Gap Message
+                                                                                T22 = Message,
+                                                                                T11 =changeClientNextMessageIdTo(IDToTransfer+1,ClientPID,NewState),  % increment counter
+                                                                                {T11,IDToTransfer,T22};
+                                                                            Message -> %Normal Message
+                                                                                T22 = Message,
+                                                                                T11 =changeClientNextMessageIdTo(IDToTransfer+1,ClientPID,NewState),% increment counter
+                                                                                {T11,IDToTransfer,T22}
+                                                                  end,
 
-                                         {IDToTransfer, MessageToTransfer} -> %ID vorhanden IDToTransfer ggf. <= RequestedIDFromClient
-                                           {T1,T2,T3} = case MessageToTransfer of
-                                                      {Message,_,_} -> %Gap Message
-                                                        T22 = Message,
-                                                        T11 =changeClientNextMessageIdTo(IDToTransfer+1,ClientPID,NewState),  % increment counter
-                                                        {T11,IDToTransfer,T22};
-                                                      Message -> %Normal Message
-                                                        T22 = Message,
-                                                        T11 =changeClientNextMessageIdTo(IDToTransfer+1,ClientPID,NewState),% increment counter
-                                                        {T11,IDToTransfer,T22}
-                                            end,
+                                                                  {T1,T2,T3}%Return
+                                                           end,
 
-                                            {T1,T2,T3}%Return
-                                       end,
-
-      RealBoolToTransfer = case RealIDToTransfer >= werkzeug:maxNrSL(TempStatus#status.dq) of
-                         true -> true;
-                         false -> false %auf True setzten für einzeln abfragen
-                       end,
+      RealBoolToTransfer = case RealIDToTransfer >= werkzeug:maxNrSL(TempStatus#status.dlq) of
+                            true -> true;
+                            false -> false %auf True setzten für einzeln abfragen
+                           end,
 
       %Return
       {TempStatus,{RealIDToTransfer,RealMessageToTransfer,RealBoolToTransfer}}
 
   end.
 
-new_message(State,Number,Nachricht) ->
-  io:format("[new_message METHODE]~n"),
-  io:format("Recived: (~w) ~s ~n",[Number,Nachricht]),
-  NachrichtAndTimeStamp = string:concat(Nachricht,werkzeug:timeMilliSecond()),
-
-  NewState = State#status{hbq = werkzeug:pushSL(State#status.hbq,{Number,NachrichtAndTimeStamp})},
-  %NewState = State#status{hbq = lists:append(State#status.hbq, {Nachricht})},
-  io:format("[NEW LIST]~p~n",[NewState#status.hbq]),
-  NewState.
 
 
 
-query_msgid(State) ->
-  io:format("[query_msgid METHODE]~n"),
-  NewState = State#status{current_msg_id = State#status.current_msg_id + 1},
-  NewState.
-
-
-maxNrSLHelper(Liste,State)->
-  case werkzeug:maxNrSL(Liste) of
-    -1 -> 0; %Bei Leerer Liste
-    _ ->
-
-      T = werkzeug:maxNrSL(Liste),
-      Return = case werkzeug:findSL(State#status.dq,T) of
-        {_,{_,_,To}} -> %Wenn hoechster = Lueckenfueller
-          %io:format("[Lueckenfueller detected] between ~p and ~p (during number: ~p)~n",[Fr,To,T]),
-          To;
-        _ ->
-          T
-      end,
-      Return
-  end.
 
 %% Uebertraegt Nachrichten von der Holdbackqueue in die Deliveryqueue.
 %% Fuellt Luecken mit einer Fehlernachricht.
-moveMessagesFromHbqToDq(State) ->
-  io:format("~n~n[moveMessagesFromHbqToDq]~n~n"),
-  NewState = case werkzeug:maxNrSL(State#status.dq) < 10 of %TODO: Use Value from Config CASE HIER: in der DQ maximal 10
+moveMessagesFromHbqToDlq(State) ->
+
+  Config = State#status.config,
+
+  NewState = case werkzeug:maxNrSL(State#status.dlq) < Config#config.dlqlimit of
     true -> %In der Deliveryqueue sind noch Plaetze frei:
 
       case werkzeug:lengthSL(State#status.hbq) >0 of
         true -> %Es sind noch Nachrichten in der Holdbackqueue (die in die Deleiveryqueue uebertragen werden koennen)
           LowestIDInHBQ = werkzeug:minNrSL(State#status.hbq),
-          io:format("[KeyToMove from HBQ to DQ (LowestIDInHBQ)] ~p~n",[LowestIDInHBQ]),
-          HighestIDinDQ = maxNrSLHelper(State#status.dq, State),
-          io:format("[HighestIDinDQ] ~p~n",[HighestIDinDQ]),
+          io:format("[KeyToMove from HBQ to DLQ (LowestIDInHBQ)] ~p~n",[LowestIDInHBQ]),
+          HighestIDinDLQ = maxNrSLHelper(State#status.dlq, State),
+          io:format("[HighestIDinDLQ] ~p~n",[HighestIDinDLQ]),
 
 
 
-          Temp3State = case LowestIDInHBQ > HighestIDinDQ+1 of %wenn lücke (min 1 elem)
-            true -> %Der naechste Eintrag in der Holdbackqueue ist groesser, als der letzte in der Deliveryqueue; Also eine Luecke die gefuellt werden muss!
+          Temp3State = case HighestIDinDLQ+1 < LowestIDInHBQ  of %wenn Luecke (von min 1 elem)
+            true -> %Der naechste Eintrag in der Holdbackqueue ist groesser, als der letzte in der Deliveryqueue; Also haben wir eine Luecke die gefuellt werden muss!
               io:format("[ID is NOT present in HBQ]~n"),
-              MessageAndTimestamp ={string:join(["****Fehlernachricht (Lueckenfueller) fuer Nachricht", werkzeug:to_String(HighestIDinDQ+1),"bis", werkzeug:to_String(LowestIDInHBQ-1),"um ", werkzeug:timeMilliSecond()]," "),HighestIDinDQ+1,LowestIDInHBQ-1},
+              MessageAndTimestamp ={string:join(["****Fehlernachricht (Lueckenfueller) fuer Nachricht", werkzeug:to_String(HighestIDinDLQ+1),"bis", werkzeug:to_String(LowestIDInHBQ-1),"um ", werkzeug:timeMilliSecond()]," "),HighestIDinDLQ+1,LowestIDInHBQ-1},
 
-              TempState =State#status{dq = werkzeug:pushSL(State#status.dq, {HighestIDinDQ+1,MessageAndTimestamp})}, %Speicher PlainText Nachricht + Lueckenstart + Lueckenende Werte in der Deliveryqueue
+              TempState =State#status{dlq = werkzeug:pushSL(State#status.dlq, {HighestIDinDLQ+1,MessageAndTimestamp})}, %Speicher PlainText Nachricht + Lueckenstart + Lueckenende Werte in der Deliveryqueue
               TempState;
 
 
             false -> %Der naechste Eintrag in der Holdbackqueue hat die MessageID, die in der Deliveryqueue erwarted wird (keine Lücke erkannt)
               io:format("[Id is in Row / ID is present in HBQ (all good)]~n"),
               {_,TempMessage} = werkzeug:findSL(State#status.hbq, LowestIDInHBQ),%Kopier naechste Nachricht aus der Holdbackqueue in Variable
-              MessageAndTimestamp = string:concat(TempMessage,werkzeug:timeMilliSecond()),
+              MessageAndTimestamp = string:join([TempMessage,"|To DLQ:",werkzeug:timeMilliSecond()]," "),
 
-              TempState =State#status{dq = werkzeug:pushSL(State#status.dq, {LowestIDInHBQ,MessageAndTimestamp})}, %Speicher die Nachricht in der Deliveryqueue
-              Temp2State =TempState#status{hbq = werkzeug:popSL(TempState#status.hbq)}, %Entferne die (jetzt alte) Nachricht aus der Holdbackqueue popSL=kleinste nummer
+              TempState = State#status{dlq = werkzeug:pushSL(State#status.dlq, {LowestIDInHBQ,MessageAndTimestamp})}, %Speicher die Nachricht in der Deliveryqueue
+              Temp2State = TempState#status{hbq = werkzeug:popSL(TempState#status.hbq)}, %Entferne die (jetzt alte) Nachricht aus der Holdbackqueue popSL=kleinste nummer
               Temp2State
 
           end,
 
 
-          io:format("State#status.dq: ~p~n", [Temp3State#status.dq]),
+          io:format("State#status.dlq: ~p~n", [Temp3State#status.dlq]),
           io:format("State#status.hbq: ~p~n", [Temp3State#status.hbq]),
-          moveMessagesFromHbqToDq(Temp3State); %Recusiver Aufruf, bis Deliveryqueue maximal gefuellt (oder keine Nachrichten mehr in der Holdbackqueue zum verschieben vorhanden)
+          moveMessagesFromHbqToDlq(Temp3State); %Recusiver Aufruf, bis Deliveryqueue maximal gefuellt (oder keine Nachrichten mehr in der Holdbackqueue zum verschieben vorhanden)
 
 
       _ -> %Die Holdbackqueue hat (z.Zt.) keine Nachrichten mehr.
@@ -206,6 +216,9 @@ moveMessagesFromHbqToDq(State) ->
   NewState.
 
 
+%% #############################################HELPER##################################################################
+
+%% Setzt einen neuen Wert für den Lese Client
 changeClientNextMessageIdTo(NewValue,ClientPID,State)->
   {ClientPID,CurrentValue} = werkzeug:findSL(State#status.clients,ClientPID),
 
@@ -213,3 +226,20 @@ changeClientNextMessageIdTo(NewValue,ClientPID,State)->
   Return = NewState#status{clients = werkzeug:pushSL(NewState#status.clients,{ClientPID,NewValue})}, %Return State mit neuem Value für Client
   io:format("[changeClientNextMessageIdTo() return]~p~n",[Return]),
   Return.
+
+
+%% Helfermethode zum ermitteln der hoechsten Id der Deliveryqueue
+maxNrSLHelper(Liste,State)->
+  case werkzeug:maxNrSL(Liste) of
+    -1 -> 0; %Bei Leerer Liste
+    _ ->
+
+      T = werkzeug:maxNrSL(Liste),
+      Return = case werkzeug:findSL(State#status.dlq,T) of
+                 {_,{_,_,To}} -> %Wenn hoechster = Lueckenfueller
+                   To; % dann geb ID wo die Luecke zuende ist zurueck
+                 _ ->
+                   T %sonst geb MaxId zurueck
+               end,
+      Return
+  end.
